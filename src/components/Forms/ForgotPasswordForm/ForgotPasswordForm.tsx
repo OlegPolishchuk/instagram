b8e2352fc3584a86f09fae4d86d12bb2c6e047ef
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-05-25 12:30:40 +0300

    fix: forgot password form Hint styles

--- src/components/Forms/ForgotPasswordForm/ForgotPasswordForm.tsx
+++ src/components/Forms/ForgotPasswordForm/ForgotPasswordForm.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, KeyboardEvent, useEffect, useState } from 'react';
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
 
 import { useTranslation } from 'next-i18next';
 import Link from 'next/link';
