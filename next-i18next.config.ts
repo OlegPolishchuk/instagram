f3d9b346207905a53b88bcc7f0c0f7e2f5645c73
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-05-30 14:30:37 +0300

    fix: next-i18next.config

--- next-i18next.config.ts
+++ next-i18next.config.ts
@@ -1,6 +1,7 @@
-import path from 'path';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 
-const config = {
+module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ru'],
@@ -8,5 +9,3 @@ const config = {
   localePath: path.resolve('./public/locales'),
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
-
-export default config;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const i18n = require('./next-i18next.config.ts');
+const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
   webpack: (config, { isServer }) => {
