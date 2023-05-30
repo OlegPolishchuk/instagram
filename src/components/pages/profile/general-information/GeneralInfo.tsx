72ca368a69ed542e3480a34c6ae574ec990df715
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-05-30 13:07:10 +0300

    feat: generalInfo main structure

--- src/components/pages/profile/general-information/GeneralInfo.tsx
+++ src/components/pages/profile/general-information/GeneralInfo.tsx
@@ -5,8 +5,13 @@ import cls from './GeneralInfo.module.css';
 import { ProfileForm } from './profileForm/ProfileForm';
 
 import { Button } from '@/shared/ui';
+import { useGetProfileQuery } from '@/store/api';
 
 export const GeneralInfo = () => {
+  const { data } = useGetProfileQuery();
+
+  console.log(data);
+
   return (
     <div className={cls.container}>
       <div className={cls.data_container}>
