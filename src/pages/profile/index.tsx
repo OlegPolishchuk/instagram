141b47a21a67edd84e44b56b7fbf1426b75ddec3
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-05-30 13:46:27 +0300

    fix: Profile page type

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import { GetServerSideProps } from 'next';
+import { GetServerSideProps, InferGetStaticPropsType } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import { getAsideNavLayout } from '@/components/Layouts';
 import { PostsList, ProfileData } from '@/components/Profile';
 import { useGetProfileQuery } from '@/store/api';
 
-const Profile = () => {
+const Profile: InferGetStaticPropsType<typeof getServerSideProps> = () => {
   const { isLoading, data } = useGetProfileQuery();
 
   console.log({ data });
