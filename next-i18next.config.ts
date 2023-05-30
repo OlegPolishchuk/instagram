de178901261424af5e71f536f9b7d1e80001b244
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-05-30 14:26:26 +0300

    fix: next-i18next.config

--- next-i18next.config.ts
+++ next-i18next.config.ts
@@ -1,7 +1,12 @@
-module.exports = {
+import path from 'path';
+
+const config = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ru'],
   },
+  localePath: path.resolve('./public/locales'),
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
+
+export default config;
