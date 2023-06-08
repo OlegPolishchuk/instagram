ad47863fe2d1c0b7d3617253189174fcb64e6f64
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-06-08 17:48:36 +0300

    fix: profile setting navigation link scroll

--- src/components/Navigations/AsideNav/NavLink/NavLink.tsx
+++ src/components/Navigations/AsideNav/NavLink/NavLink.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { forwardRef, ReactNode } from 'react';
 
 import clsx from 'clsx';
 import Link from 'next/link';
@@ -12,27 +12,31 @@ interface Props {
   icon?: ReactNode;
   className?: string;
   activeClassName?: string;
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement>) => void;
+  id?: string;
 }
-export const NavLink = ({ href, children, icon, className, activeClassName }: Props) => {
-  const router = useRouter();
-  const currentPath = router.pathname;
+export const NavLink = forwardRef<HTMLAnchorElement, Props>(
+  ({ href, children, icon, onClick, id, className, activeClassName }, ref) => {
+    const router = useRouter();
+    const currentPath = router.pathname;
 
-  const isActive = (href: string) => {
-    const pathNameList = currentPath.split('/');
-    const endpoint = `/${href.split('/').reverse()[0]}`;
-    const parentPath = `/${pathNameList[1]}`;
+    const isActive = (href: string) => {
+      const pathNameList = currentPath.split('/');
+      const endpoint = `/${href.split('/').reverse()[0]}`;
+      const parentPath = `/${pathNameList[1]}`;
 
-    return endpoint === parentPath || currentPath === href;
-  };
+      return endpoint === parentPath || currentPath === href;
+    };
 
-  const LinkCLassName = isActive(href)
-    ? clsx(cls.link, className, cls.active, activeClassName)
-    : clsx(cls.link, className);
+    const LinkCLassName = isActive(href)
+      ? clsx(cls.link, className, cls.active, activeClassName)
+      : clsx(cls.link, className);
 
-  return (
-    <Link href={href} className={LinkCLassName}>
-      {icon && <div className={cls.icon}>{icon}</div>}
-      <span>{children}</span>
-    </Link>
-  );
-};
+    return (
+      <Link href={href} className={LinkCLassName} onClick={onClick} id={id} ref={ref}>
+        {icon && <div className={cls.icon}>{icon}</div>}
+        <span>{children}</span>
+      </Link>
+    );
+  },
+);
--- src/components/Navigations/ProfileSettingsNav/ProfileSettingsNav.tsx
+++ src/components/Navigations/ProfileSettingsNav/ProfileSettingsNav.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import { useTranslation } from 'next-i18next';
 
@@ -6,6 +6,7 @@ import cls from './ProfileSettingNav.module.css';
 
 import { NavLink } from '@/components';
 import { Routes } from '@/shared/constants';
+import { useMediaQuery } from '@/shared/hooks';
 
 interface SettingsRoutes {
   route: string;
@@ -22,14 +23,45 @@ const settingRoutes: SettingsRoutes[] = Object.entries(Routes.Profile.settings).
 export const ProfileSettingsNav = () => {
   const { t } = useTranslation('profile');
 
+  const isSmallScreen = useMediaQuery('(max-width: 768px)');
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const activeLinkRef = useRef<HTMLAnchorElement>(null);
+  const activeLinkItemRef = useRef<HTMLDivElement>(null);
+
+  const containerWidth = isSmallScreen ? containerRef?.current?.offsetWidth : '';
+
+  const handleLinkClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    if (!isSmallScreen) {
+      return;
+    }
+
+    if (containerRef.current && containerWidth) {
+      const navItem = event.target as HTMLAnchorElement;
+
+      const containerRect = containerRef.current.getBoundingClientRect();
+      const navItemRect = navItem.getBoundingClientRect();
+
+      const navItemOffset = navItemRect.left - containerRect.left;
+      const containerCenter = containerWidth / 2;
+      const navItemCenter = navItemOffset + navItemRect.width / 2;
+
+      const scrollLeft = navItemCenter - containerCenter;
+
+      containerRef.current.scrollLeft += scrollLeft;
+    }
+  };
+
   return (
-    <nav className={cls.nav}>
+    <nav className={cls.nav} ref={containerRef}>
       {settingRoutes.map(route => (
-        <div key={route.path} className={cls.nav_item}>
+        <div key={route.path} className={cls.nav_item} ref={activeLinkItemRef}>
           <NavLink
             className={cls.link}
             activeClassName={cls.link_active}
             href={route.path}
+            ref={activeLinkRef}
+            onClick={handleLinkClick}
           >
             {t(`settings.${route.route}`)}
           </NavLink>
