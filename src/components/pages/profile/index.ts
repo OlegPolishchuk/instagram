f976a27d38bcdd10d51a267887b1fefa6cb710c0
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-06-08 13:41:54 +0300

    fix: payments page index paths

--- src/components/pages/profile/index.ts
+++ src/components/pages/profile/index.ts
@@ -1,4 +1,4 @@
 export { GeneralInfo } from './general-information/GeneralInfo';
-export { ActiveDevices, CurrentDevice } from './devices';
+export { CurrentDevice, ActiveDevices } from './devices';
 export { AccountOptions } from './account-management/AccountOptions';
 export { Payments } from './payments/Payments';
